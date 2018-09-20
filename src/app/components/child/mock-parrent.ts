0166fc289badd9a1bc1cf6e73dc3bbe604cfe9bb
youssef maghzaz <youssefmaghzaz@gmail.com>
2018-09-20 21:22:45 +0200

    enhance display code

--- src/app/components/child/mock-parrent.ts
+++ src/app/components/child/mock-parrent.ts
@@ -1,9 +1,5 @@
-import * as _ from "lodash";
-
 const mockParrent = (mockedComponent, mockedMethod) => {
-  console.log(mockedMethod);
   for (let key in mockedMethod) {
-    console.log(typeof mockedComponent[key]);
     if (typeof mockedComponent[key] === "function") {
       spyOn(mockedComponent, key).and.returnValue(mockedMethod[key]);
     } else if (typeof mockedComponent[key] === "object") {
